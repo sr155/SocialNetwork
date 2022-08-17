b0f37988e346a718439e938fa162d624fc37407a
sr155 <104543440+sr155@users.noreply.github.com>
2022-08-17 17:57:15 +0530

    initial

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MembersComponent } from './members/members.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component:HomeComponent},
+  {path:'profile',component:ProfileComponent},
+  {path:'members',component:MembersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
